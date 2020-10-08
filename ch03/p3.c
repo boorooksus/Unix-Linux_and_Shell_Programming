41a95e18bf5b77c95dc478750e877f418d824087
boorooksus <boorooksus@gmail.com>
2020-10-08 18:17:05 +0900

    octls함수 반복문으로 간소화, lsoct함수 비트연산자 활용

--- ch03/p3.c
+++ ch03/p3.c
@@ -2,93 +2,55 @@
 #include <unistd.h>
 #include <fcntl.h>
 
-char ls[10];
+char ls[10]; /* 변환한 permission 문자열 저장 */
 
 int lsoct(char perm[]){
-	int res = 0;
-	
-	for(int i = 0; perm[i] != '\0'; i += 3){
-		int temp = 0;
-		for(int j = 2; j >= 0; j--){
-			if (perm[i + 2 - j] != '-'){
-				int sqrt = 1;
-				for(int k = 0; k < j; k++){
-					sqrt *= 2;
-				}
-				temp = temp + sqrt;
-				
-			}
+	/* 비트 연산자를 이용하여 이진수로 변환 후 리턴 */
+	int bi = 0b0;
+	int cur = 0b1;
+	for(int i = 8; i > -1; i--){
+		/* '-'가 아닌 경우 1, 맞는 경우 0 */
+		if(perm[i] != '-'){
+			bi |= cur;
 		}
-		res = res * 10 + temp;
+		cur <<= 1;
 	}
-	
-	return res;
+	return bi;
 }
 
 char *octls(char* perm){
-	int oc = atoi(perm);
+	int oc = atoi(perm); /* permission 10진법 정수로 변경 */
+	int i = 0; /* ls 인덱스 */
 	
-	int u = oc / 100;
-	oc %= 100;
-	if(u >= 4){
-		ls[0] = 'r';
-		u -= 4;
-	} else{
-		ls[0] = '-';
-	}
-	if(u >= 2){
-		ls[1] = 'w';
-		u -= 2;
-	} else{
-		ls[1] = '-';
-	}
-	if(u >= 1){
-		ls[2] = 'x';
-	} else{
-		ls[2] = '-';
-	}
-	
-	int g = oc / 10;
-	oc %= 10;
-	if(g >= 4){
-		ls[3] = 'r';
-		g -= 4;
-	} else{
-		ls[3] = '-';
-	}
-	if(g >= 2){
-		ls[4] = 'w';
-		g -= 2;
-	} else{
-		ls[4] = '-';
-	}
-	if(g >= 1){
-		ls[5] = 'x';
-	} else{
-		ls[5] = '-';
-	}
-	
-	int o = oc;
-	if(o >= 4){
-		ls[6] = 'r';
-		o -= 4;
-	} else{
-		ls[6] = '-';
-	}
-	if(o >= 2){
-		ls[7] = 'w';
-		o -= 2;
-	} else{
-		ls[7] = '-';
-	}
-	if(o >= 1){
-		ls[8] = 'x';
-	} else{
-		ls[8] = '-';
+	/* oc를 백의 자리수부터 문자열로 변환 */
+	int mod = 100; 
+	while (oc > 0){
+		int cur = oc / mod;
+		oc %= mod;
+		mod /= 10;
+		
+		/* ls 각 자리에 해당하는 문자 저장 */
+		if(cur >= 4){
+			ls[i] = 'r';
+			cur -= 4;
+		} else{
+			ls[i] = '-';
+		}
+		if(cur >= 2){
+			ls[i + 1] = 'w';
+			cur -= 2;
+		} else{
+			ls[i + 1] = '-';
+		}
+		if(cur >= 1){
+			ls[i + 2] = 'x';
+		} else{
+			ls[i + 2] = '-';
+		}
+		i += 3;
 	}
 
-	return ls;
-		
+	return ls;		
 }
 
 int main(int argc, char *argv[]){
@@ -97,13 +59,15 @@ int main(int argc, char *argv[]){
 		printf("Wrong input. Usage : %s <lsoct or octls> <permission>\n", argv[0]);
 		return 0;
 	}
-	
+	/* lsoct를 실행 */
 	if(argv[1][0] == 'l'){
-		printf("%d\n", lsoct(argv[2]));
+		printf("%o\n", lsoct(argv[2]));
 	}
+	/* octls를 실행 */
 	else if(argv[1][0] == 'o'){	
 		printf("%s\n", octls((argv[2])));
 	}
+	/* 잘못된 입력 */
 	else{
 		printf("Wrong input\n");
 	}
